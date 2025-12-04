d4aace14b5903d75756c5a7b670a41a42b515366
AlexB-png <alexander.brankin.466@accesscreative.ac.uk>
2025-12-04 01:07:33 +0000

    coding sector

---
+++ script.js
@@ -0,0 +1,84 @@
+function Email() {
+  const obfuscated = [97,108,101,120,97,110,100,101,114,46,98,114,107,110,64,112,114,111,116,111,110,46,109,101]
+  alert(String.fromCharCode(...obfuscated))
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+  console.log("Yes!")
+  const CodeLine = document.getElementById("codeBar")
+  
+  const TextInput1 = "ls"
+  const TextInput1Response = "Portfolio  Bin  Downloads  yay"
+
+  const TextInput2 = "cd Portfolio"
+
+  const TextInput3 = "Portfolio.sh"
+
+  let current = "AlexB@Arch ~ $ "
+  CodeLine.innerHTML = current
+  await sleep(1000)
+  
+  for (let i=0; i < TextInput1.length; i++) {
+    current += TextInput1[i]
+    CodeLine.innerHTML = current
+    await sleep(150)
+  }
+  current += "<br>"
+  for (let i=0; i < TextInput1Response.length; i++) {
+    current += TextInput1Response[i]
+    CodeLine.innerHTML = current
+    await sleep(3)
+  }
+  current += "<br>" + "AlexB@Arch ~ $ "
+  CodeLine.innerHTML = current
+  for (let i=0; i < TextInput2.length; i++) {
+    current += TextInput2[i]
+    CodeLine.innerHTML = current
+    await sleep(100)
+  }
+  current += "<br>" + "AlexB@Arch Portfolio $ "
+  CodeLine.innerHTML = current
+  await sleep(100)
+
+  for (let i=0; i < TextInput1.length; i++) {
+    current += TextInput1[i]
+    CodeLine.innerHTML = current
+    await sleep(150)
+  }
+  current += "<br>"
+  await sleep(500)
+  current += "Portfolio.sh"
+  CodeLine.innerHTML = current
+
+  current += "<br>" + "AlexB@Arch Portfolio $ "
+  CodeLine.innerHTML = current
+  await sleep(300)
+  
+  for (let i=0; i < TextInput3.length; i++) {
+    current += TextInput3[i]
+    CodeLine.innerHTML = current
+    await sleep(150)
+  }
+
+  current += "<br>" + "<br>" + "Press Spacebar to continue"
+  CodeLine.innerHTML = current
+
+})
+
+function HandleSpacebar(event) {
+  if (event.code == "Space"){
+  const codeDiv = document.getElementById("code")
+  codeDiv.style.display = "none"
+
+  const contentDiv = document.getElementById("content")
+  contentDiv.style.display = "block"
+
+  document.removeEventListener("keydown", HandleSpacebar);
+  }
+}
+
+document.addEventListener("keydown", HandleSpacebar);
